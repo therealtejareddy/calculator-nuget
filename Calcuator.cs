825d27ecf68682a7a6a64f538fb5d9e63fb61b1f
therealtejareddy <yuvatejareddy003@gmail.com>
2024-05-02 16:52:38 +0530

    Add project files.

---
+++ Calcuator.cs
@@ -0,0 +1,12 @@
+﻿using calculator_nuget.Contracts;
+
+namespace calculator_nuget
+{
+    public class Calcuator : ICalculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Contracts/ICalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_nuget.Contracts
+{
+    internal interface ICalculator
+    {
+        public int Add(int x, int y);
+    }
+}
